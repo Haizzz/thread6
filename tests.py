c5ec4f98472c04d6fb598f61cfd221ccdf255f85
Anh <anhvietle.info@gmail.com>
2018-08-18 20:58:04 +0800

    add run in thread function test

--- tests.py
+++ tests.py
@@ -29,6 +29,28 @@ class Test_pp(unittest.TestCase):
         # await_output should return function return
         self.assertTrue(a.await_output())
 
+    def test_run_in_thread(self):
+        # should do the same thing as the threaded decorator
+        result = []
+
+        def append_x(arr):
+            time.sleep(1)
+            arr.append("x")
+            return True
+
+        # start the threaded function call
+        a = _pp.run_in_thread(append_x, result)
+        result.append("y")
+        # y should be appended first then x since main thread
+        # is not waiting for x to finish
+        self.assertEqual(result[0], "y")
+        time.sleep(1)
+        # x should now be appended
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[1], "x")
+        # await_output should return function return
+        self.assertTrue(a.await_output())
+
 
 if __name__ == '__main__':
     unittest.main()
