721cba2b06e74a69120b9e9340504c9c82e42b7c
Anh <anhvietle.info@gmail.com>
2018-08-18 10:29:45 +0800

    add test for threaded decorator

---
+++ tests.py
@@ -0,0 +1,34 @@
+import unittest
+import time
+import _pp
+
+
+class Test_pp(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_threaded_decorator(self):
+        result = []
+
+        @_pp.threaded(False)
+        def append_x(arr):
+            time.sleep(1)
+            arr.append("x")
+            return True
+
+        # start the threaded function call
+        a = append_x(result)
+        result.append("y")
+        # y should be appended first then x since main thread
+        # is not waiting for x to finish
+        self.assertEqual(result[0], "y")
+        time.sleep(1)
+        # x should now be appended
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[1], "x")
+        # await_output should return function return
+        self.assertTrue(a.await_output())
+
+
+if __name__ == '__main__':
+    unittest.main()
