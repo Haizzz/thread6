95fdf87215138f8015a536ac4989cf86e706c547
Anh <anhvietle.info@gmail.com>
2018-08-19 00:35:47 +0800

    more misc changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thread6",
-    version="0.1.0",
+    version="0.2.0",
     author="Anh Le",
     author_email="hi@imanhle.com",
     description="A plug n play multithreading interface",
